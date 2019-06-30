eae4994c511289a7e4e133941a8099ae0e0b7e6c
Duane <duaneyaiser@comcast.net>
2019-06-30 18:48:24 -0400

    add to forecast data, add temp highs and lows to forecast, removed apparent temp.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,8 +18,10 @@ const forecast = (latitude, longitude, callback) => {
 				body.daily.data[0].summary +
 					' It is currently ' +
 					body.currently.temperature +
-					' degrees out, with the apparent temperature at ' +
-					body.currently.apparentTemperature +
+					' degrees out, with a high of ' +
+					body.daily.data[0].temperatureHigh +
+					' degrees, and a low of ' +
+					body.daily.data[0].temperatureLow +
 					' degrees. There is a ' +
 					body.currently.precipProbability +
 					'% chance of rain. ' +
